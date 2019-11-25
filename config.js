b09a47055e35ac393ee45a2614f972b7418c3f4e
Danimal-63 <daniel_guerrero_73@yahoo.com>
2019-11-24 23:16:12 -0800

    Added Level 2

--- config.js
+++ config.js
@@ -69,7 +69,17 @@ var PLATFORMS = [
 [
   {xpt:50, ypt:75 , xl: 25, yl: 25,},
   {xpt: 0,   ypt:300,  xl: 600, yl: 200},
-]
+  {xpt:275, ypt:0, xl:25, yl:25},
+  {xpt:275, ypt:25, xl:25, yl:25},
+  {xpt:275, ypt:50, xl:25, yl:25},
+  {xpt:275, ypt:75, xl:25, yl:25},
+  {xpt:275, ypt:100, xl:25, yl:25},
+  {xpt:275, ypt:125, xl:25, yl:25},
+  {xpt:275, ypt:150, xl:25, yl:25},
+  {xpt:275, ypt:175, xl:25, yl:25},
+
+
+  ]
 ];
 var HAZARDS = [[
   {xpt:0, ypt:150,xl:25,yl:25 },
@@ -88,15 +98,23 @@ var HAZARDS = [[
   {xpt:50, ypt:250, xl:25, yl:25},
   {xpt:50, ypt:275, xl:25, yl:25},
   {xpt:75, ypt:275, xl:25, yl:25},
-  {xpt:150, ypt:75, xl:25, yl:25},
-  {xpt:150, ypt:100, xl:25, yl:25},
-  {xpt:150, ypt:125, xl:25, yl:25},
-  {xpt:150, ypt:150, xl:25, yl:25},
-  {xpt:150, ypt:175, xl:25, yl:25},
-  {xpt:150, ypt:200, xl:25, yl:25},
-  {xpt:150, ypt:225, xl:25, yl:25},
-  {xpt:150, ypt:250, xl:25, yl:25},
-  {xpt:150, ypt:275, xl:25, yl:25},
+  {xpt:175, ypt:75, xl:25, yl:25},
+  {xpt:175, ypt:100, xl:25, yl:25},
+  {xpt:175, ypt:125, xl:25, yl:25},
+  {xpt:175, ypt:150, xl:25, yl:25},
+  {xpt:175, ypt:175, xl:25, yl:25},
+  {xpt:175, ypt:200, xl:25, yl:25},
+  {xpt:175, ypt:225, xl:25, yl:25},
+  {xpt:175, ypt:250, xl:25, yl:25},
+  {xpt:175, ypt:275, xl:25, yl:25},
+  {xpt:400, ypt:125, xl:25, yl:25},
+  {xpt:400, ypt:150, xl:25, yl:25},
+  {xpt:400, ypt:175, xl:25, yl:25},
+  {xpt:400, ypt:200, xl:25, yl:25},
+  {xpt:400, ypt:225, xl:25, yl:25},
+  {xpt:400, ypt:275, xl:25, yl:25},
+  {xpt:400, ypt:300, xl:25, yl:25},
+  {xpt:400, ypt:250, xl:25, yl:25},
 
 
 ]
@@ -104,7 +122,9 @@ var HAZARDS = [[
 var TUNA = [[
   {xpt:400, ypt:50, xl:25, yl: 25, collected: false}
 ],
-[{xpt:0,ypt:150,xl:25,yl:25}],
+[{xpt:0,ypt:150,xl:25,yl:25},
+{xpt:300, ypt:100, xl:25, yl:25},
+]
 ]
 
 var TEMPPLATFORMS = [
